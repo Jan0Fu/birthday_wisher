f5989fed4dfe7f661d6890076e551813965414c4
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-12-10 11:58:18 +0100

    Finished main functionality

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-##################### Hard Starting Project ######################
-
-# 1. Update the birthdays.csv with your friends & family's details. 
-# HINT: Make sure one of the entries matches today's date for testing purposes. 
-
-# 2. Check if today matches a birthday in the birthdays.csv
-# HINT 1: Only the month and day matter. 
-# HINT 2: You could create a dictionary from birthdays.csv that looks like this:
-# birthdays_dict = {
-#     (month, day): data_row
-# }
-#HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
-# if (today_month, today_day) in birthdays_dict:
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT: https://www.w3schools.com/python/ref_string_replace.asp
-
-# 4. Send the letter generated in step 3 to that person's email address.
-# HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
 import smtplib
 import datetime as dt
 import pandas
@@ -25,22 +6,25 @@ import random
 my_email = "appbreweryinfo@gmail.com"
 password = "abcd1234()"
 now = dt.datetime.now()
-day = now.day
+today = (now.month, now.day)
 date_of_birth = dt.datetime(year=1988, month=5, day=3)
 
 data = pandas.read_csv("birthdays.csv")
-data.to_dict()
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 
-if date_of_birth == day:
-    with open("quotes.txt") as file:
-        content = file.readlines()
+if today in birthdays_dict:
+    birthday_person = birthdays_dict[today]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as file:
+        content = file.read()
+        content = content.replace("[NAME]", birthday_person["name"])
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(user=my_email, password=password)
         connection.sendmail(
             from_addr=my_email,
-            to_addrs="appbrewerytesting@yahoo.com",
-            msg=f"Subject:Hello\n\n{random.choice(content)}"
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday\n\n{content}"
         )
 
 
